872c080902c12b426ebf4df92455b74439474b71
FeiyangJin <31809915+FeiyangJin@users.noreply.github.com>
2018-05-31 11:48:08 -0500

    Add files via upload

--- PythonCSV/Lidar_IMU_frequency_optimize.py
+++ PythonCSV/Lidar_IMU_frequency_optimize.py
@@ -20,50 +20,53 @@ timeDiffer = []
 rowList = []
 
 def getIMU():
+    global imu
+    lib.lsm9ds1_readAccel(imu)
+    ax = lib.lsm9ds1_getAccelX(imu)
+    ay = lib.lsm9ds1_getAccelY(imu)
+    az = lib.lsm9ds1_getAccelZ(imu)
+    cax = lib.lsm9ds1_calcAccel(imu, ax)
+    cay = lib.lsm9ds1_calcAccel(imu, ay)
+    caz = lib.lsm9ds1_calcAccel(imu, az)
+    gx = lib.lsm9ds1_getGyroX(imu)
+    gy = lib.lsm9ds1_getGyroY(imu)
+    gz = lib.lsm9ds1_getGyroZ(imu)
+    cgx = lib.lsm9ds1_calcGyro(imu, gx)
+    cgy = lib.lsm9ds1_calcGyro(imu, gy)
+    cgz = lib.lsm9ds1_calcGyro(imu, gz)
+    return (cax,cay,caz,cgx,cgy,cgz)
+
+
+def getLidar():
+    #TFmini data
+    recv = ser.read(9)
+    ser.reset_input_buffer()
+
+    if recv[0] == 0x59 and recv[1] == 0x59:
+        distance = recv[2] + recv[3] * 256
+        ser.reset_input_buffer()
+        return distance
     
 def getData():
     global lasttime
-    #connect with IMU
-    imu = lib.lsm9ds1_create()
-    lib.lsm9ds1_begin(imu)
-    if lib.lsm9ds1_begin(imu) == 0:
-        print("Failed to communicate with LSM9DS1.")
-        quit()
-    lib.lsm9ds1_calibrate(imu)
+    global imu
 
     current = time.time()
     while current - startTime < 10:
         
         current = time.time()
         if lib.lsm9ds1_accelAvailable(imu) > 0 and ser.in_waiting > 8:
-            lib.lsm9ds1_readAccel(imu)
-            ax = lib.lsm9ds1_getAccelX(imu)
-            ay = lib.lsm9ds1_getAccelY(imu)
-            az = lib.lsm9ds1_getAccelZ(imu)
-            cax = lib.lsm9ds1_calcAccel(imu, ax)
-            cay = lib.lsm9ds1_calcAccel(imu, ay)
-            caz = lib.lsm9ds1_calcAccel(imu, az)
-            gx = lib.lsm9ds1_getGyroX(imu)
-            gy = lib.lsm9ds1_getGyroY(imu)
-            gz = lib.lsm9ds1_getGyroZ(imu)
-            cgx = lib.lsm9ds1_calcGyro(imu, gx)
-            cgy = lib.lsm9ds1_calcGyro(imu, gy)
-            cgz = lib.lsm9ds1_calcGyro(imu, gz)
-            #TFmini data
-            recv = ser.read(9)
-            ser.reset_input_buffer()
-
-            if recv[0] == 0x59 and recv[1] == 0x59:
-                distance = recv[2] + recv[3] * 256
-                ser.reset_input_buffer()
-                currentTime = str(datetime.datetime.now()); #timestamp data
-                row = [currentTime,distance,cax,cay,caz,cgx,cgy,cgz] #the row being written to csv file, just x and y accel
-                rowList.append(row)
-                diff = time.time() - lasttime
-                timeDiffer.append(diff)
-                #print(diff)
-                lasttime = time.time()
-                #print(row)
+            IMUdata = getIMU()
+            Lidardata = getLidar()
+            
+            currentTime = str(datetime.datetime.now()); #timestamp data
+            row = [currentTime,Lidardata,IMUdata] #the row being written to csv file, just x and y accel
+            rowList.append(row)
+            diff = time.time() - lasttime
+            timeDiffer.append(diff)
+            #print(diff)
+            lasttime = time.time()
+            #print(row)
 
     print("start writing data")
     with open(datafile,"a",newline = '') as csvfile:
@@ -82,7 +85,16 @@ def getData():
     
 startTime = time.time()
 lasttime = time.time()
+#connect with IMU
+imu = lib.lsm9ds1_create()
+lib.lsm9ds1_begin(imu)
+
+
 if __name__ == '__main__':
+    if lib.lsm9ds1_begin(imu) == 0:
+        print("Failed to communicate with LSM9DS1.")
+        quit()
+    lib.lsm9ds1_calibrate(imu)
     try:
         if ser.is_open == False:
             ser.open()
